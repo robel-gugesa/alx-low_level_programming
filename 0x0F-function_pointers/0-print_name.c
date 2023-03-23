2da32faa58ab14c99060705b7a16f2c418f62333
robel-gugesa <robelgugesa@gmail.com>
2023-03-22 23:39:20 -0700

    Update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,7 +7,7 @@
  *@f: pointer to function
  *Return: nothing
  **/
-void print_name(char *name, void(*f)(char*))
+void print_name(char *name, void(*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
